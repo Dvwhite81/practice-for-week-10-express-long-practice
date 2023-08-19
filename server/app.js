0b3e66866b841f01ba480bdaffd8b1a81efd51d4
Dan <dvwhite1981@gmail.com>
2023-08-18 22:15:12 -0400

    Basic Phase 2 - Complete

--- server/app.js
+++ server/app.js
@@ -9,6 +9,17 @@ app.use('/static', express.static('assets'));
 // For parsing application/json
 app.use(express.json());
 
+// Logger middleware
+app.use((req, res, next) => {
+  console.log("Method:", req.method);
+  console.log("URL:", req.url);
+
+  res.on('finish', () => {
+    console.log("Status Code:", res.statusCode);
+  });
+  next();
+});
+
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -25,6 +36,13 @@ app.post('/test-json', (req, res, next) => {
 // For testing express-async-errors
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
+});
+
+// Resource Not Found
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.statusCode = 404;
+  next(err);
 });
 
 const port = 5000;
