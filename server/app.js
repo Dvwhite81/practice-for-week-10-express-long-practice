9257ddaf400997f8b7757c24617a6cd07cff568f
Dan <dvwhite1981@gmail.com>
2023-08-18 23:58:33 -0400

    Phase 4 - Complete

--- server/app.js
+++ server/app.js
@@ -11,11 +11,11 @@ app.use(express.json());
 
 // Logger middleware
 app.use((req, res, next) => {
-  console.log("Method:", req.method);
-  console.log("URL:", req.url);
+  console.log('Method:', req.method);
+  console.log('URL:', req.url);
 
   res.on('finish', () => {
-    console.log("Status Code:", res.statusCode);
+    console.log('Status Code:', res.statusCode);
   });
   next();
 });
@@ -26,12 +26,12 @@ app.use('/dogs', dogsRouter);
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
-  res.json("Express server running. No content provided at root level. Please use another route.");
+  res.json('Express server running. No content provided at root level. Please use another route.');
 });
 
 // For testing express.json middleware
 app.post('/test-json', (req, res, next) => {
-  // send the body as JSON with a Content-Type header of "application/json"
+  // send the body as JSON with a Content-Type header of 'application/json'
   // finishes the response, res.end()
   res.json(req.body);
   next();
@@ -39,15 +39,35 @@ app.post('/test-json', (req, res, next) => {
 
 // For testing express-async-errors
 app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+  throw new Error('Hello World!')
 });
 
 // Resource Not Found
 app.use((req, res, next) => {
-  const err = new Error("The requested resource couldn't be found.");
+  const err = new Error('The requested resource couldn\'t be found.');
   err.statusCode = 404;
   next(err);
 });
 
-const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+// Error handling middleware
+app.use('/', (err, req, res, next) => {
+  console.error(err);
+  res.statusCode = err.statusCode || 500;
+  let response = {
+    'message': err.message || 'Something went wrong',
+    'statusCode': res.statusCode || 500
+  }
+  if (env !== "production") {
+    response.stack = err.stack;
+  }
+  res.json(response);
+});
+
+const port = process.env.PORT;
+const env = process.env.NODE_ENV;
+const secret = process.env.SECRET_MESSAGE;
+app.listen(port, () => {
+  console.log('Server is listening on port', port);
+  console.log('Secret message:', secret);
+  console.log('Environment:', env);
+});
